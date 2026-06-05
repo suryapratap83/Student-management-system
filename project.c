239154dee606e916e1c0e5443b48684a9f3f0bc0
suryapratap83 <suryapratap8315@gmail.com>
2026-06-06 00:32:21 +0530

    Initial setup of struct student

---
+++ project.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct student {
+    int rollno;
+    char name[20];
+    float marks;
+    char course[20];
+} student;
+
+student arr[50];
+int count = 0;
+
+int main() {
+    return 0;
+}
