a5f66a983cedbf25f3574a462c6a48510a6cd88e
suryapratap83 <suryapratap8315@gmail.com>
2026-06-10 21:59:54 +0530

    Refactored — moved all logic into separate functions

--- project.c
+++ project.c
@@ -1,126 +1,148 @@
-#include<stdio.h>
-    typedef struct student{
-        int rollno;
-        char name[20];
-        float marks;
-        char course[20];
-    }student;
-    student arr[50];
-int main(){
-int menu;
-  int count =0;
-do{
-    printf("\n1.Add");
-    printf("\n2.View");
-    printf("\n3.delete");
-    printf("\n4.search");
-    printf("\n5.update");
-    printf("\n6.exit");
+#include <stdio.h>
+typedef struct student
+{
+    int rollno;
+    char name[20];
+    float marks;
+    char course[20];
+} student;
+student arr[50];
+int count = 0;
+void addstudents()
+{
 
-    printf("\nEnter menu:");
-    scanf("%d", &menu);
-    switch(menu){
-        case 1: 
-      
     printf("\nEnter Roll No: ");
     scanf("%d", &arr[count].rollno);
 
     printf("\nEnter Name: ");
-    scanf(" %s", arr[count].name);
+    scanf("  %[^\n]", arr[count].name);
 
     printf("\nEnter Marks: ");
     scanf("%f", &arr[count].marks);
 
     printf("\nEnter Course: ");
-    scanf("%s", arr[count].course);
+    scanf(" %[^\n]", arr[count].course);
 
-    count++; 
+    count++;
 
     printf("Student added successfully!");
+}
+void viewstudents()
+{
+    for (int i = 0; i < count; i++)
+    {
+        printf("\n%d", arr[i].rollno);
 
-    break;
-        
-        case 2:
-       for(int i=0; i<count; i++){
-    printf("\n%d", arr[i].rollno);
+        printf("\n%s", arr[i].name);
 
-    printf("\n%s", arr[i].name);
+        printf("\n%f", arr[i].marks);
 
-    
-    printf("\n%f", arr[i].marks);
-
-    printf("\n%s", arr[i].course);
-       }
-
-        break;
-         case 3:{
-        int rollno;
-        printf("\nenter rollno");
-        scanf("%d", &rollno);
-        for(int i=0; i<count; i++){
-            if(arr[i].rollno==rollno){
-                for(int j=i; j<count-1; j++){
-                    arr[j]=arr[j+1];
+        printf("\n%s", arr[i].course);
+    }
+}
+void deletestudents()
+{
+    int rollno;
+    printf("\nenter rollno");
+    scanf("%d", &rollno);
+    for (int i = 0; i < count; i++)
+    {
+        if (arr[i].rollno == rollno)
+        {
+            for (int j = i; j < count - 1; j++)
+            {
+                arr[j] = arr[j + 1];
             }
             count--;
         }
     }
-        printf("student deleted succesfully");
-        break;
+    printf("student deleted succesfully");
 }
-        case 4:{
-            int  rollno;
-        printf("\nenter rollno");
-        scanf("%d", &rollno);
-        for(int i=0; i<count; i++){
-            if(arr[i].rollno==rollno){
-             printf("%d", arr[i].rollno);
+void searchstudents()
+{
+    int rollno;
+    printf("\nenter rollno");
+    scanf("%d", &rollno);
+    for (int i = 0; i < count; i++)
+    {
+        if (arr[i].rollno == rollno)
+        {
+            printf("%d", arr[i].rollno);
 
-    printf("%s", arr[i].name);
+            printf("%s", arr[i].name);
 
-    
-    printf("%f", arr[i].marks);
+            printf("%f", arr[i].marks);
 
-    printf(" %s", arr[i].course);
+            printf(" %s", arr[i].course);
         }
     }
-       
-        break;
 }
-     case 5:{
-           int  rollno;
-        printf("\nenter rollno");
-        scanf("%d", &rollno);
-        for(int i=0; i<count; i++){
-            if(arr[i].rollno==rollno){
-             
-    printf("\nEnter Roll No: ");
-    scanf("%d", &arr[i].rollno);
+void updatestudents()
+{
+    int rollno;
+    printf("\nenter rollno");
+    scanf("%d", &rollno);
+    for (int i = 0; i < count; i++)
+    {
+        if (arr[i].rollno == rollno)
+        {
 
-    printf("\nEnter Name: ");
-    scanf(" %s", arr[i].name);
+            printf("\nEnter Roll No: ");
+            scanf("%d", &arr[i].rollno);
 
-    printf("\nEnter Marks: ");
-    scanf("%f", &arr[i].marks);
+            printf("\nEnter Name: ");
+            scanf("  %[^\n]", arr[i].name);
 
-    printf("\nEnter Course: ");
-    scanf("%s", arr[i].course);
+            printf("\nEnter Marks: ");
+            scanf(" %[^\n]", &arr[i].marks);
+
+            printf("\nEnter Course: ");
+            scanf(" %[^\n]", arr[i].course);
         }
-  
+    }
+    printf("student update succesfully");
 }
-printf("student update succesfully");
-  break;
+int main()
+{
+    int menu;
 
-     }
+    do
+    {
+        printf("\n1.Add");
+        printf("\n2.View");
+        printf("\n3.delete");
+        printf("\n4.search");
+        printf("\n5.update");
+        printf("\n6.exit");
+
+        printf("\nEnter menu:");
+        scanf("%d", &menu);
+        switch (menu)
+        {
+        case 1:
+            addstudents();
+            break;
+
+        case 2:
+            viewstudents();
+            break;
+        case 3:
+            deletestudents();
+            break;
+
+        case 4:
+            searchstudents();
+            break;
+        case 5:
+            updatestudents();
+            break;
+
         case 6:
-        printf("exiting....");
-        break;
-        default :
-        printf("invalid choice");
-    
-        
-    
-    }
-}while(menu!=6);
-    
+            printf("exiting....");
+            break;
+        default:
+            printf("invalid choice");
+        }
+    } while (menu != 6);
 }
+
